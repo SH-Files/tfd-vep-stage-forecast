9586782bf1dc684e07c5b4680ff0ea47c0703255
SH-Files <72735178+SH-Files@users.noreply.github.com>
2025-08-02 21:54:40 +0200

    Added stage A picture, smal logic change for countdown formatting

--- src/utils/formatting.tsx
+++ src/utils/formatting.tsx
@@ -1,12 +1,12 @@
 const zeroPad = (n: number) => (n < 10 ? `0${n}` : `${n}`);
 
 export const formatCountDown = (ms: number) => {
-  const totalSeconds = Math.floor(ms / 1000);
+  const totalSeconds = Math.abs(Math.floor(ms / 1000));
 
-  const days = Math.floor(Math.abs(totalSeconds) / 86400);
-  const hours = Math.floor((Math.abs(totalSeconds) % 86400) / 3600);
-  const minutes = Math.floor((Math.abs(totalSeconds) % 3600) / 60);
-  const seconds = Math.abs(totalSeconds) % 60;
+  const days = Math.floor(totalSeconds / 86400);
+  const hours = Math.floor((totalSeconds % 86400) / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
 
   return `${zeroPad(days)}:${zeroPad(hours)}:${zeroPad(minutes)}:${zeroPad(
     seconds
